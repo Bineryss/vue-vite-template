ef65191de0017a36cb4fc12c7eb5753873d17c36
Sebastian Otto <44194014+Bineryss@users.noreply.github.com>
2024-06-05 20:41:52 +0000

    fix: lint staged

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,21 +3,17 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    'plugin:prettier/recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    'plugin:prettier/recommended'
+    // '@vue/eslint-config-prettier/skip-formatting'
   ],
   overrides: [
     {
-      files: [
-        'e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:playwright/recommended'
-      ]
+      files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+      extends: ['plugin:playwright/recommended']
     }
   ],
   parserOptions: {
